39711be04b95071665e3dcbe72b6ebda7deeecc7
Adhib Arfan <adhib.arfan@gmail.com>
2017-02-16 23:45:27 +0700

    5. enum jenis barang

---
+++ src/main/java/com/arfan/adhib/ecommerce/domain/JenisBarang.java
@@ -0,0 +1,16 @@
+package com.arfan.adhib.ecommerce.domain;
+
+/**
+ *
+ * @Author AdhibArfan <adhib.arfan@gmail.com>
+ * @Web <https://adhibarfan.github.io>
+ * @Since Feb 16, 2017
+ * @Time 10:17:56 PM
+ * @Encoding UTF-8
+ * @Project Spring-Ecommerce
+ * @Package com.arfan.adhib.ecommerce.domain
+ *
+ */
+public enum JenisBarang {
+    cair, padat, gas
+}
