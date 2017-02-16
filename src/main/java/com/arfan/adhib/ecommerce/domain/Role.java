8df4603697b0ede1ff932d0778d3c89c0da09614
Adhib Arfan <adhib.arfan@gmail.com>
2017-02-16 23:46:23 +0700

    8. enum role

---
+++ src/main/java/com/arfan/adhib/ecommerce/domain/Role.java
@@ -0,0 +1,16 @@
+package com.arfan.adhib.ecommerce.domain;
+
+/**
+ *
+ * @Author AdhibArfan <adhib.arfan@gmail.com>
+ * @Web <https://adhibarfan.github.io>
+ * @Since Feb 16, 2017
+ * @Time 10:27:21 PM
+ * @Encoding UTF-8
+ * @Project Spring-Ecommerce
+ * @Package com.arfan.adhib.ecommerce.domain
+ *
+ */
+public enum Role {
+    ROLE_ADMIN, ROLE_USER, ROLE_CLIENT
+}
