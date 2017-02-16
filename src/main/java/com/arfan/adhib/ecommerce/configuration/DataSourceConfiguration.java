33496a5d0e5d7e4e508d6955630200d2bb8dbc20
Adhib Arfan <adhib.arfan@gmail.com>
2017-02-16 23:49:00 +0700

    15. datasource configuration

---
+++ src/main/java/com/arfan/adhib/ecommerce/configuration/DataSourceConfiguration.java
@@ -0,0 +1,49 @@
+package com.arfan.adhib.ecommerce.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ *
+ * @Author AdhibArfan <adhib.arfan@gmail.com>
+ * @Web <https://adhibarfan.github.io>
+ * @Since Feb 16, 2017
+ * @Time 10:06:12 PM
+ * @Encoding UTF-8
+ * @Project Spring-Ecommerce
+ * @Package com.arfan.adhib.ecommerce.configuration
+ *
+ */
+
+@Configuration
+@PropertySource("classpath:application.yml")
+public class DataSourceConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(environment.getRequiredProperty("spring.datasource.driver-class-name"));
+        dataSourceConfig.setJdbcUrl(environment.getRequiredProperty("spring.datasource.url"));
+        dataSourceConfig.setUsername(environment.getRequiredProperty("spring.datasource.username"));
+        dataSourceConfig.setPassword(environment.getRequiredProperty("spring.datasource.password"));
+        dataSourceConfig.setMaximumPoolSize(Integer.parseInt(environment.getRequiredProperty("spring.datasource.maximumPoolSize")));
+        dataSourceConfig.setMinimumIdle(Integer.parseInt(environment.getRequiredProperty("spring.datasource.minimumIdle")));
+        dataSourceConfig.setMaxLifetime(Integer.parseInt(environment.getRequiredProperty("spring.datasource.maxLifetime")));
+        dataSourceConfig.setConnectionTimeout(Long.parseLong(environment.getRequiredProperty("spring.datasource.connectionTimeout")));
+        dataSourceConfig.setIdleTimeout(Long.parseLong(environment.getRequiredProperty("spring.datasource.idleTimeout")));
+        dataSourceConfig.addDataSourceProperty("poolName", environment.getRequiredProperty("spring.datasource.poolName"));
+        dataSourceConfig.addDataSourceProperty("cachePrepStmts", "true");
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        return new HikariDataSource(dataSourceConfig);
+    }
+}
